aac1877530af66aad249cad3d69bc5912208da5b
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-13 12:58:54 -0300

    estrutura base da ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Provider as ReduxProvider } from "react-redux"
-import { AddTodo } from "./components/AddTodo"
-import { TodoList } from "./components/TodoList"
 import { store } from "./store"
+import { Player } from "./pages/Player"
+
+import './styles/global.css'
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
+      <Player />
     </ReduxProvider>
   )
 }
