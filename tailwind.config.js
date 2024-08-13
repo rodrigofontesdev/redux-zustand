5c4aa9c67f26d839c4de8635356e9e6766292820
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-13 15:27:47 -0300

    separando componentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,5 +7,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar')
+  ],
 }
